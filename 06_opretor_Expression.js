8dc4de0da3642167dac8894de100ca0af6c2341f
Arpit Patoliya <154015590+PatoliyaArpit@users.noreply.github.com>
2024-02-05 23:33:28 +0530

    Create 06_opretor_Expression.js

---
+++ 06_opretor_Expression.js
@@ -0,0 +1,65 @@
+//arithmetic operator
+let a=10;
+let b=4;
+let c=6;
+
+// console.log("sum =",a+b)
+// console.log("sub =",a-b)
+// console.log("divison =",a/b)
+// console.log("mul =",a*b)
+// console.log("% =",a%b)
+// console.log("a++ =",a++)//second time number print after number increas show
+// console.log("a-- =",a--)
+// console.log("b++",b++)
+// console.log("sum =",b)
+// console.log("++c",++c)//increment show first increas number and second number show
+
+
+//assignment op 
+
+let e=10;
+e +=5;//assigment is use to main value in increas
+
+console.log(e)
+
+//comparison op
+
+
+let com1=6;
+let com2=7;
+console.log("comp1==comp2",com1==com2)
+console.log("comp1!=comp2",com1!=com2)
+console.log("comp1===comp2",com1===com2)
+console.log("comp1!==comp2",com1!==com2)
+console.log("comp1>comp2",com1>com2)
+
+//logical op
+
+let x=5;
+let y=6;
+console.log(x<y && x==5)
+console.log(x<y || x==7)
+console.log(!false)
+console.log(!true)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
