499a2599977f3eb9251d822c15dac9c6934d3476
Arpit Patoliya <154015590+PatoliyaArpit@users.noreply.github.com>
2024-02-05 23:32:45 +0530

    Create 05_practice.js

---
+++ 05_practice.js
@@ -0,0 +1,32 @@
+//q1
+let a="Arpit"
+let b="6"
+console.log(a+b)
+
+//q2
+
+console.log(typeof(a+b))
+
+//q3
+const sch = {
+    Name:"Arpit",
+    section:1
+}
+// sch="kenil"// string is a not change
+
+//q4 const in object is a change
+sch["Friend"]="rutil"//add in key valu not change for variable  this sch is a constant in a object create this name and other item change
+sch["Name"]="Arpit1"
+
+console.log(sch)
+
+//q5 dictionary
+
+const dict ={
+name:"Arpit",
+Collage:"CU shah",
+from:"Ahemdabad",
+living:"shree saurash patel samaj",
+cource:"bca"
+}
+console.log(dict["living"])
