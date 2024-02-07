477bffff68d4dcdcc06f410e7d8b06d434505b6c
Arpit Patoliya <154015590+PatoliyaArpit@users.noreply.github.com>
2024-02-07 11:09:04 +0530

    Create c2_practice.js

---
+++ c2_practice.js
@@ -0,0 +1,48 @@
+//chapter 2 practice
+//priblem 1
+/*
+let age = 18;
+if (age > 10 && age < 20) {
+  console.log("your age bettwen 10 and 20");
+} else {
+  console.log("invalid");
+}
+*/
+
+//problem 2
+/*
+let exp = 1;
+
+switch (exp) {
+  case 1:
+    console.log("hello");
+}
+*/
+
+//problem 3
+/*
+let a = 20;
+
+if (a % 3 == 0 && a % 2 == 0) {
+  console.log("your number is divisible by 2 and 3");
+} else {
+  console.log("your number is not divisible by 2 and 3");
+}
+*/
+//problem4
+/*
+let a = 20;
+
+if (a % 3 == 0 || a % 2 == 0) {
+  console.log("your number is divisible by 2 and 3");
+} else {
+  console.log("your number is not divisible by 2 and 3");
+}
+*/
+
+//problem 5
+let a = 16;
+let b = ("you can", a > 18 ? " drive" : " not drive");
+//console.log("you can", a > 18 ? "drive" : "not drive");//ternary op
+
+console.log(b);
