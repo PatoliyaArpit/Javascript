43d990aeebb99779d80d2b18574a69619318d645
Arpit Patoliya <154015590+PatoliyaArpit@users.noreply.github.com>
2024-02-07 11:09:54 +0530

    Create 08_loops.js

---
+++ 08_loops.js
@@ -0,0 +1,29 @@
+//loops
+// add firstb no
+
+let sum = 0;
+let n;
+for (let i = 0; i < 500; i++) {
+  sum += i;
+}
+console.log(sum);
+// console.log(i);
+
+/*
+let obj = {
+  Arpit: 90,
+  kenil: 60,
+  rutil: 75,
+  pavan: 70,
+};
+//console.log(obj["pavan"]);
+for (let a in obj) {
+  //for in loop
+  // console.log(obj[a]); // only keys print
+  console.log("mark of " + a + " are " + obj[a]);
+}
+//for of loop
+for (let b of "Arpit") {
+  console.log(b);
+}
+*/
