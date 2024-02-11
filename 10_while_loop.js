0c06b6307e7cf9050b0932a68c2a34eae7be6800
Arpit Patoliya <154015590+PatoliyaArpit@users.noreply.github.com>
2024-02-11 14:24:56 +0530

    Create 10_while_loop.js

---
+++ 10_while_loop.js
@@ -0,0 +1,19 @@
+//Do while loop
+/*
+let a = 5;
+a = Number.parseInt(a);
+let i = 0;
+while (i < a) {
+  console.log(i);
+  i++;
+}
+*/
+
+//do while loop
+let b = 7;
+let i = 0;
+b = Number.parseInt(b);
+do {
+  console.log(i);
+  i++;
+} while (i < b);
